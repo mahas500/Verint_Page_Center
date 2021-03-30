e66d539d6722f30ecb12c4a91584dfc36f384ac1
mahas500 <10518579@mydbs.ie>
2021-03-30 20:00:11 +0100

    some additions

--- VerintTest.java
+++ VerintTest.java
@@ -7,43 +7,34 @@ import java.util.Scanner;
 public class VerintTest {
 
 
-    public static List<Integer> getPages(int currentPage, int totalPagesOfResult, int maxRange)
-    {
+    public static List<Integer> getPages(int currentPage, int totalPagesOfResult, int maxRange) {
+
         int center;
         int start;
         int end = 0;
 
         List<Integer> listOfPages = new ArrayList<Integer>(totalPagesOfResult);
 
-        if(totalPagesOfResult <= maxRange)
-        {
-            for(int i = 1; i <= totalPagesOfResult; i++)
-            {
+        if (totalPagesOfResult <= maxRange) {
+            for (int i = 1; i <= totalPagesOfResult; i++) {
                 listOfPages.add(i);
             }
-        }
-
-        else
-        {
-            center = maxRange/2 + 1;
+        } else {
+            center = maxRange / 2 + 1;
 
             start = currentPage - center + 1;
 
-            end = (maxRange%2 == 0) ? currentPage + center - 2 : currentPage + center - 1;
+            end = (maxRange % 2 == 0) ? currentPage + center - 2 : currentPage + center - 1;
 
-            if(start < 1)
-            {
+            if (start < 1) {
                 start = 1;
                 end = start + maxRange - 1;
-            }
-            else if(end > totalPagesOfResult)
-            {
+            } else if (end > totalPagesOfResult) {
                 end = totalPagesOfResult;
                 start = totalPagesOfResult - maxRange + 1;
             }
 
-            for(int i = start; i <= end; i++)
-            {
+            for (int i = start; i <= end; i++) {
                 listOfPages.add(i);
             }
 
@@ -55,8 +46,8 @@ public class VerintTest {
 
         boolean status = true;
 
-        while(status)
-        {
+        while(status) {
+
             Scanner input = new Scanner(System.in);
 
             System.out.println("Enter total Pages");
@@ -67,25 +58,57 @@ public class VerintTest {
 
             int maxRange = input.nextInt();
 
-            if(totalPagesOfResult < 1 || maxRange < 1)
-            {
-                System.out.println("Only natural numbers are allowed");
-            }
-            else
-            {
-                for(int i = 1; i <= totalPagesOfResult; i++)
-                {
-                    System.out.println(getPages(i,totalPagesOfResult,maxRange));
+            if (totalPagesOfResult < 1 || maxRange < 1) {
+
+                System.out.println("Only positive integer values are allowed");
+
+            } else {
+                System.out.println("Do you wish to enter a specific page number or entire list ?");
+
+                System.out.println("Please press 1 for specific page result or 2 to get the entire list");
+
+                String strPref = input.next();
+
+                if (strPref.equalsIgnoreCase("1")) {
+
+                    System.out.println("Please enter your desired page number");
+
+                    int currentPage = input.nextInt();
+
+                    if(currentPage < 1 || currentPage > totalPagesOfResult)
+                    {
+                        System.out.println("Please enter a valid page number from 1 to total pages entered value");
+                        currentPage = input.nextInt();
+                        System.out.println(getPages(currentPage, totalPagesOfResult, maxRange));
+                    }
+                    else{
+
+                        System.out.println(getPages(currentPage, totalPagesOfResult, maxRange));
+                    }
+
+                } else if (strPref.equalsIgnoreCase("2")) {
+
+                    for (int i = 1; i <= totalPagesOfResult; i++) {
+
+                        System.out.println(getPages(i, totalPagesOfResult, maxRange));
+                    }
+                } else {
+
+                    System.out.println("Please enter a valid input ");
+
+                    status = true;
                 }
+
             }
 
             System.out.println("Do you want to enter more values ? ");
-            System.out.println("Please press No to terminate or any other key to continue ");
+
+            System.out.println("Please press 0 to terminate or any other key to continue ");
 
             String str = input.next();
 
-            if(str.equalsIgnoreCase("No"))
-            {
+            if (str.equalsIgnoreCase("0")) {
+
                 status = false;
 
             }
